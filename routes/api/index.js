516aba63095d7a52236dce7b020d49c9b3e84ea5
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-06-22 21:24:18 -0400

    updated api/index.js to include thought routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,14 @@
 // import all of the api routes to prefix their enpoint name and package them up
 
 const router = require('express').Router();
+const thoughtRoutes = require('./thought-routes');
 const userRoutes = require('./user-routes');
 
-// add prefix of /users to routes creates in user-routes.js
+// add prefix of /thoughts to routes created in thought-routes.js
+router.use('/thoughts', thoughtRoutes);
+
+// add prefix of /users to routes created in user-routes.js
 router.use('/users', userRoutes);
+
 
 module.exports = router;
